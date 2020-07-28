11f224d8b1d773c967b9122b168340e0650fb491
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-28 10:57:36 +0900

    Added show controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::MoviesController < Api::V1::BaseController
+  before_action :set_movie, only: [:show]
+
   def index
     @movies = Movie.all
     render json: @movies
+  end
+
+  def show
+    render json: @movie
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 end
